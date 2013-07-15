9415fd08f4d209ce8e25fd3ee23bedbade99b533
unaunagi <unaunagi@gmx.com>
2013-07-16 02:06:15 +0900

    一応完成したということで

--- NumberPaste.py
+++ NumberPaste.py
@@ -3,19 +3,25 @@ import re
 
 class NumberPasteCommand(sublime_plugin.TextCommand):
     def run(self, edit):
+        #クリップボードの中身を取得する
         clip = sublime.get_clipboard()
-        if re.search('\$\d+\$', clip):
-            #$$の部分を外してからペースト
-            paste = re.sub('\$(\d+)\$', r'\1', clip)
+        #選択中の文字列を取得する(複数選択は未対応)
+        select_txt = self.view.substr(self.view.sel()[0])
+        #書式が正しいか調べる
+        m = re.match('(\d+)-(\d+)',select_txt)
+        if m:
+            #書式は正しいので、番号を書き換えながら連続ペーストする
+            start = int(m.group(1))
+            end = int(m.group(2))
+            paste = "";
+            for i in range(start,end+1):
+                paste += re.sub('\$\$\$', str(i), clip)
+            #最後にまとめてペースト
             self.view.replace(edit, self.view.sel()[0], paste)
-            sels.clear()
-            #$$の中身を1づつ増やす
-            #数字が違うものが混ざっているかもしれないから、慎重に
-            #tmp = re.split('\$(\d+)\$', clip)
-
-
         else:
-            #そのままコピー
-            self.view.replace(edit, self.view.sel()[0], clip)
-            sels.clear()
+            #想定する書式と違っていたら、何もせず終了
+            #(エラーメッセージを出したい。英語で、日本人にもわかりそうなのを)
+            pass
+
+
 
