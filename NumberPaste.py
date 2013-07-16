dbe6e78c8dbb371c9e4212325fe395339471806b
unaunagi <unaunagi@gmx.com>
2013-07-16 13:56:53 +0900

    コメントの訂正

--- NumberPaste.py
+++ NumberPaste.py
@@ -9,10 +9,10 @@ class NumberPasteCommand(sublime_plugin.TextCommand):
         clip = sublime.get_clipboard()
         #選択中の文字列を取得する(複数選択は未対応)
         select_txt = self.view.substr(self.view.sel()[0])
-        #書式が正しいか調べる
+        #書式が連番指定になっているか調べる
         m = re.match('(\d+)-(\d+)',select_txt)
         if m:
-            #書式は正しいので、番号を書き換えながら連続ペーストする
+            #書式が正しいので、番号を書き換えながら連続ペーストする
             start = int(m.group(1))
             end = int(m.group(2))
             paste = "";
@@ -21,7 +21,7 @@ class NumberPasteCommand(sublime_plugin.TextCommand):
             #最後にまとめてペースト
             self.view.replace(edit, self.view.sel()[0], paste)
         else:
-            #想定する書式と違っていたら、何もせず終了
+            #想定する書式と違っているので、何もせず終了
             #(エラーメッセージを出したい。英語で、日本人にもわかりそうなのを)
             pass
 
